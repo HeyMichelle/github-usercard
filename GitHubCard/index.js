accfe2b4e4cb0cd674afc5d3bce3f0844be41c35
michelle-hey <miscmiche@gmail.com>
2020-02-20 13:27:53 -0500

    Function component created.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,14 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+           https://api.github.com/users/HeyMichelle
 */
+axios.get('https://api.github.com/users/HeyMichelle')
+  .then( response => {
+    console.log(response);// deal with the response data in here
+  })
+  .catch( err => {
+    console.log("Error:", err);
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -26,6 +33,57 @@
 
 const followersArray = [];
 
+const userCard = document.querySelector('.card');
+function createCard(data) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+    const avatarImg = document.createElement('img');
+    card.appendChild(avatarImg);
+    avatarImg.src = `${data.avatar_url}`;
+
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  card.appendChild(cardInfo);
+
+    const name = document.createElement('h3');
+    name.classList.add('name');
+    name.textContent = `${data.name}`;
+    cardInfo.appendChild(name);
+
+    const username = document.createElement('p');
+    username.classList.add('username');
+    username.textContent = `${data.login}`;
+    cardInfo.appendChild(username);
+
+    const location = document.createElement('p');
+    location.textContent = "Location: ", `${data.location}`;
+    cardInfo.appendChild(location);
+
+    const profile = document.createElement('p');
+    profile.textContent = "Profile: ", `${data.html_url}`;
+    cardInfo.appendChild(profile);
+
+      const pageLink = document.createElement('a');
+      profile.appendChild(pageLink);
+
+    const followers = document.createElement('p');
+    followers.textContent = "Followers: ", `${data.followers}`;
+    cardInfo.appendChild(followers);
+
+    const following = document.createElement('p');
+    following.textContent = "Following: ", `&{data.following}`;
+    cardInfo.appendChild(following);
+
+    const bio = document.createElement('p');
+    bio.textContent = "Bio: ", `${data.bio}`;
+    cardInfo.appendChild(bio);
+
+    console.log(card);
+    return card;
+}
+
+userCard.appendChild(card);
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
